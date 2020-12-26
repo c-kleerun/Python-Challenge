95f36af750912e5d878101629a88d22c5153d695
c-klee-run <kristy.allred@gmail.com>
2020-12-25 20:54:15 -0500

    added code to total months and added table to display complete data

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -1,7 +1,23 @@
 import os
 import csv
 
-bank = os.path.join('Resources', 'budget_data.csv')
+path = os.path.join('Resources', 'budget_data.csv')
 
-with open(bank) as csvfile:
+with open(path) as bank:
     csvreader = csv.reader(bank, delimiter=',')
+    header = next(csvreader)
+    print(header)
+
+    row_count = sum(1 for row in csvreader)
+    print(row_count)
+    #
+
+
+# total_months = x
+# for x in row[0]
+#     x = (x+1)
+
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {row_count}")
+# print(f"")

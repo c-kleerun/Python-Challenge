8f71f94661d54e899b40863872a560376ca79b68
c-klee-run <kristy.allred@gmail.com>
2020-12-29 20:10:29 -0500

    changed code in pypoll_main.py

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -7,23 +7,43 @@ with open(path) as election:
     csvreader = csv.reader(election, delimiter=',')
     header = next(csvreader)
     
-    row_count = sum(1 for row in csvreader)
-    
-    name:[]
+    vote_count = 0
+    names = []
+    votes = {}
     for row in csvreader:
-        if row not in name:
-            name.append(row)
-        print(name)
+        vote_count = vote_count+1
+        if row[2] not in names:
+            names.append(row[2])
+            votes[row[2]] = 0
+
+        votes[row[2]] = votes[row[2]]+1 
+        # votes[khan]=1 +1
+        
+    # for x in votes:    
+        
+
+    # print(names)
+    print(votes)
 
 
 
+# def total_votes (name, percent):
+#         if x in name:
+#             return name[percent]
+
+#     votes['_found'] = total_votes
+
+# k = total_votes["Khan"]
+# c = total_votes["Correy"]
+# l = total_votes["Li"]
+# o = total_votes["O'Tooley"]
 
 
 print('Election Results')
 print('--------------------')
-print(f'Total Votes: {row_count}')
+print(f'Total Votes: {vote_count}')
 print('---------------------')
-# print('Khan:', 'Correy: ', 'Li: ', "O'Tooley: " )
+# print("{k} \n{c} \n{l} \n{o}")
 # print('---------------------')
 # print('Winner: {}')
 # print('---------------------')

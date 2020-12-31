92e48e40ef5137bcac0d87020c496ceb786cdbec
c-klee-run <kristy.allred@gmail.com>
2020-12-30 22:24:24 -0500

    added percent calculation and votes print cmd

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -8,8 +8,10 @@ with open(path) as election:
     header = next(csvreader)
     
     vote_count = 0
+    i = 0
     names = []
     votes = {}
+    vote_percent = {}
     for row in csvreader:
         vote_count = vote_count+1
         if row[2] not in names:
@@ -19,19 +21,37 @@ with open(path) as election:
         votes[row[2]] = votes[row[2]]+1 
         # votes[khan]=1 +1
         
-    # for x in votes: 
-    #     vote_percent = votes[1]/vote_count
-    #     print(vote_percent)
+    for k,v in votes.items():
+        pct = v * 100/vote_count
+        if k in votes:
+            votes[k].append(pct)
+        
+        # print(k, (round(pct)))
 
-    print(names)
-    print(votes)
+    # print(names)
+    # print(votes)
     
 
 print('Election Results')
 print('--------------------')
 print(f'Total Votes: {vote_count}')
 print('---------------------')
-# print("{k} \n{c} \n{l} \n{o}")
-# print('---------------------')
+print(f'Khan: {votes.get("Khan")}')
+# print(f'Correy: {(round(pct, 2))}%, {votes.get("Correy")}')
+# print(f'Li: {(round(pct, 2))}%, {votes.get("Li")}')
+# print("O'Tooley:" + {(round(pct,2))} + {votes.get("O'Tooley")})
+    # Correct formatting? 
+    # There needs to be both single and double quotes to get this to work...
+print('---------------------')
 # print('Winner: {}')
 # print('---------------------')
+
+# with open(os.path.join('Analysis/election_results.txt'), 'w') as txt:
+#     txt.writelines('Election Results\n')
+#     txt.writelines('------------------\n')
+#     txt.writelines(f'Total Votes: {vote_count}\n')
+#     txt.writelines('-------------------------\n')
+#     txt.writelines()
+#     txt.writelines()
+#     txt.writelines()
+#     txt.writelines()
